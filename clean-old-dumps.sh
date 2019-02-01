5c2c2212726b424bcddb6ef9eb6653a5e68ec8a0
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-02-01 13:42:17 -0600

    Update clean-old-dumps.sh

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -u root -pBLAZE flare -e 'delete from dumps where time < date_sub(curdate(), interval 7 day); delete from dump_info where time < date_sub(curdate(), interval 7 day); commit;'
+mysql -u root -pBLAZE flare -e 'delete from dumps where time < date_sub(curdate(), interval 1 hour); delete from dump_info where time < date_sub(curdate(), interval 1 hour); commit;'
