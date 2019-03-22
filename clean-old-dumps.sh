493bc552ff31fb391469f4115b125439fb79d61f
Blaze User <blaze@localhost.localdomain>
2019-03-22 13:33:05 -0500

    Update dump cleaner

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -u root -pBLAZE flare -e 'delete from dumps where time < date_sub(curdate(), interval 1 hour); delete from dump_info where time < date_sub(curdate(), interval 1 hour); commit;'
+mysql -u root -pBLAZE flare -e 'delete from dump_info where time < date_sub(curdate(), interval 1 hour); delete from dumps where time < date_sub(curdate(), interval 1 hour); delete from dns_dump where timestamp < date_sub(curdate(), interval 1 hour); commit;'
