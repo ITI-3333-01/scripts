fbf9c541675d9f7dd95bb3f1126106ae972202fe
clo90 <45404537+clo90@users.noreply.github.com>
2019-03-22 13:36:00 -0500

    Update clean-old-dumps.sh

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -u root -pBLAZE flare -e 'delete from dump_info where time < date_sub(curdate(), interval 1 hour); delete from dumps where time < date_sub(curdate(), interval 1 hour); delete from dns_dump where timestamp < date_sub(curdate(), interval 1 hour); commit;'
+mysql -u root -pBLAZE flare -e 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
