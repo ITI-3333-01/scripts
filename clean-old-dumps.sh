ac19ca00ef767d8c457f40d6953ff1aa572def11
Blaze User <blaze@localhost.localdomain>
2019-03-29 09:28:54 -0500

    Add data count dump to clean old dumps

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mysql -u root -pBLAZE flare -e 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
+mysql -u root -pBLAZE flare -e > /home/blaze/vclean.out 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
+
+date >> /home/blaze/vclean.out
+
+mysql -u root -pBLAZE flare -e >> /home/blaze/vclean.out 'select count(*) from dumps; select count(*) from dump_info; select count(*) from dns_dump; commit;'
