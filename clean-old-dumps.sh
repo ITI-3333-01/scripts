30fac3233599396162211676c98e31c9891ac6ef
tmyatt <47039331+tmyatt@users.noreply.github.com>
2019-04-16 14:24:40 -0500

    Update clean-old-dumps.sh

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mysql -u root -pBLAZE flare -e > /home/blaze/vclean.out 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
+mysql -u flare -pFlare-3333 flare -e > /home/blaze/vclean.out 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
 
 date >> /home/blaze/vclean.out
 
-mysql -u root -pBLAZE flare -e >> /home/blaze/vclean.out 'select count(*) from dumps; select count(*) from dump_info; select count(*) from dns_dump; commit;'
+mysql -u flare -pFlare-3333 flare -e >> /home/blaze/vclean.out 'select count(*) from dumps; select count(*) from dump_info; select count(*) from dns_dump; commit;'
