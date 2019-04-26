75e59330851408ef00b6315da0be1cbbf63b9bbd
Austin Mayes <bookofnathan@gmail.com>
2019-04-26 12:49:36 -0500

    Change DNS time column name

--- clean-old-dumps.sh
+++ clean-old-dumps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql -u flare -pFlare-3333 flare -e > /home/blaze/vclean.out 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where timestamp < date_sub(now(), interval 1 day); commit;'
+mysql -u flare -pFlare-3333 flare -e > /home/blaze/vclean.out 'delete from dump_info where time < date_sub(now(), interval 1 day); delete from dumps where time < date_sub(now(), interval 1 day); delete from dns_dump where time < date_sub(now(), interval 1 day); commit;'
 
 date >> /home/blaze/vclean.out
 
